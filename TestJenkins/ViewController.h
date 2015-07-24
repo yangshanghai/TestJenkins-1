8bc197535125284b223009188f3501874ac644ba
xuanyi0627 <xuanyicode@icloud.com>
2015-07-24 21:48:40 +0800

    create project

---
+++ TestJenkins/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestJenkins
+//
+//  Created by 宣佚 on 15/7/24.
+//  Copyright (c) 2015年 宣佚. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestJenkins/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TestJenkins
+//
+//  Created by 宣佚 on 15/7/24.
+//  Copyright (c) 2015年 宣佚. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
